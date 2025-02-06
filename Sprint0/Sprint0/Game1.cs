ff25e89813149d5aeff7da0e688336a7e3dc09c0
Cody Yang <cody.yang@cantatahealth.com>
2025-02-05 19:17:48 -0500

    Reset Repository

---
+++ Sprint0/Sprint0/Game1.cs
@@ -0,0 +1,85 @@
+﻿using CSE3902;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace CSE3902
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private ISprite currentSprite;
+        private IController mouseController;
+        private IController keyboardController;
+        private float scaleFactor;
+        private SpriteFont gameFont;
+        private string creditsText = "Cody Yang - Art Source: Carmen Canvas Mario";
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            scaleFactor = 3f;
+        }
+
+        protected override void Initialize()
+        {
+            keyboardController = new KeyboardController(this);
+            mouseController = new MouseController(this); 
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            var marioTexture = Content.Load<Texture2D>("Sprite");
+            currentSprite = new StaticSprite(marioTexture, new Rectangle(0, 10, 16,18), scaleFactor);
+            gameFont = Content.Load<SpriteFont>("Arial"); 
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            keyboardController.Update(gameTime); // ✅ Ensure Update uses GameTime
+            mouseController.Update(gameTime);
+            currentSprite.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            // Draw current sprite
+            currentSprite.Draw(spriteBatch);
+
+            if (gameFont != null)
+            {
+                spriteBatch.DrawString(gameFont, creditsText, new Vector2(50, 400), Color.White);
+            }
+
+            Texture2D rect = new Texture2D(GraphicsDevice, 100, 20);
+            Color[] data = new Color[100 * 20];
+            for (int i = 0; i < data.Length; ++i) data[i] = Color.Black;
+            rect.SetData(data);
+
+            spriteBatch.Draw(rect, new Vector2(250, 450), Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+
+        public void SetSprite(ISprite newSprite)
+        {
+            currentSprite = newSprite;
+        }
+    }
+}
---
+++ Sprint0/Sprint0/MouseController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using CSE3902;
+
+namespace CSE3902
+{
+    public class MouseController : IController
+    {
+        private Game1 game;
+
+        public MouseController(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var mouseStatus = Mouse.GetState();
+            float scaleFactor = 4f;
+
+            // Exit on right-click
+            if (mouseStatus.RightButton == ButtonState.Pressed)
+            {
+                game.Exit();
+            }
+            else if (mouseStatus.LeftButton == ButtonState.Pressed)
+            {
+                int width = game.GraphicsDevice.Viewport.Width;
+                int height = game.GraphicsDevice.Viewport.Height;
+
+                if (mouseStatus.X < width / 2 && mouseStatus.Y < height / 2)
+                {
+                    // Top Left - Static Sprite
+                    game.SetSprite(new StaticSprite(
+                        game.Content.Load<Texture2D>("Sprite"),
+                        new Rectangle(0, 10, 16, 18),
+                        scaleFactor
+                    ));
+                }
+                else if (mouseStatus.X >= width / 2 && mouseStatus.Y < height / 2)
+                {
+                    // Top Right - Animated Sprite
+                    game.SetSprite(new AnimatedSprite(
+                        game.Content.Load<Texture2D>("Sprite"),
+                        new List<Rectangle>
+                        {
+                            new Rectangle(35, 11, 16, 16),
+                            new Rectangle(51, 11, 16, 16)
+                        },
+                        0.1f,
+                        scaleFactor
+                    ));
+                }
+                else if (mouseStatus.X < width / 2 && mouseStatus.Y >= height / 2)
+                {
+                    // Bottom Left - Moving Sprite
+                    game.SetSprite(new MovingSprite(
+                        game.Content.Load<Texture2D>("Sprite"),
+                        new Rectangle(0, 10, 16, 18),
+                        scaleFactor
+                    ));
+                    Console.WriteLine("Clicked!");
+                }
+                else if (mouseStatus.X >= width / 2 && mouseStatus.Y >= height / 2)
+                {
+                    // Bottom Right - Moving Animated Sprite
+                    game.SetSprite(new MovingAnimatedSprite(
+                        game.Content.Load<Texture2D>("Sprite"),
+                        new List<Rectangle>
+                        {
+                            new Rectangle(35, 11, 16, 16),
+                            new Rectangle(51, 11, 16, 16)
+                        },
+                        0.1f,
+                        scaleFactor
+                    ));
+                }
+            }
+        }
+    }
+}
