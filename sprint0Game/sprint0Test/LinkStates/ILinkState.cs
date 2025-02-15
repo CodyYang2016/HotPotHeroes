83169e456cd78b16bc135ec90ccc9ee45161ecc9
Larry Di <113941754+larry2099@users.noreply.github.com>
2025-02-15 00:49:37 -0500

    handle Link’s state transitions

---
+++ sprint0Game/sprint0Test/LinkStates/ILinkState.cs
@@ -0,0 +1,6 @@
+public interface ILinkState
+{
+	void Enter(Link link);
+	void Update(Link link);
+	void Exit(Link link);
+}
---
+++ sprint0Game/sprint0Test/LinkStates/LinkWalkingState.cs
@@ -0,0 +1,11 @@
+public class LinkWalkingState : ILinkState
+{
+    public void Enter(Link link)
+    {
+        link.sprite.SetState(LinkAction.Walking, link.CurrentDirection);
+    }
+
+    public void Update(Link link) { }
+
+    public void Exit(Link link) { }
+}
