d8bdff0e80bea59e08db6dd47e7710ce61b31be5
Larry Di <113941754+larry2099@users.noreply.github.com>
2025-02-27 22:28:54 -0500

    Fixed version for bin

---
+++ SprintTestFixedNew/Projectiles/Fireball.cs
@@ -0,0 +1,22 @@
+﻿using BlockBranch.sprint0Game.sprint0Test.Projectiles;
+using HotpotHeroes.sprint0Game.sprint0Test.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BlockBranch.sprint0Game.sprint0Test.Projectiles
+{
+    public class Fireball : AbstractProjectile, IProjectile
+    {
+        public Fireball(Vector2 startPosition, Vector2 direction)
+            : base(startPosition, direction, TextureManager.Instance.GetTexture("Fireball"), 300f)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            // Additional logic if needed (e.g., fireball explosion on impact)
+        }
+    }
+
+}
