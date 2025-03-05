2054a3382e04fdaf2d3a8b2eb5eaa8d936f9ad7a
Larry Di <di.51@osu.edu>
2025-03-05 15:21:04 -0500

    Added a temp right wall to prevent Link from going through

--- SprintTest/Blocks/BlockList.cs
+++ SprintTest/Blocks/BlockList.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
@@ -25,6 +26,7 @@ public class BlockSprites
     private int currentIndex = 0; // The current index for managing blocks
 
     // Constructor to initialize blocks
+    /*
     public BlockSprites(Texture2D dungeonTexture)
     {
         // Add blocks to the game objects list with the texture and rectangle
@@ -39,6 +41,46 @@ public class BlockSprites
         gameObjects.Add(new Block(dungeonTexture, dragon, ObjectType.Rock, new Vector2(100, 80), 3f));
         gameObjects.Add(new BlockStair(dungeonTexture, stair, new Vector2(100, 80), 3f));
     }
+    */
+    public BlockSprites(Texture2D dungeonTexture)
+    {
+        int roomWidth = 17;  // Number of tiles in width
+        int roomHeight = 11; // Number of tiles in height
+        int tileSize = 43;   // Each tile is 48x48 pixels
+        Vector2 roomStart = new Vector2(0, 0); // Starting position of the room
+
+        // Define door positions (adjust as needed)
+        Vector2 topDoor = new Vector2(roomStart.X + (roomWidth / 2) * tileSize, roomStart.Y);
+        Vector2 bottomDoor = new Vector2(roomStart.X + (roomWidth / 2) * tileSize, roomStart.Y + (roomHeight - 1) * tileSize);
+        Vector2 leftDoor = new Vector2(roomStart.X, roomStart.Y + (roomHeight / 2) * tileSize);
+        Vector2 rightDoor = new Vector2(roomStart.X + (roomWidth - 1) * tileSize, roomStart.Y + (roomHeight / 2) * tileSize);
+
+        // Add brick blocks around the edges, skipping door positions
+        for (int x = 0; x < roomWidth; x++)
+        {
+            Vector2 topPos = new Vector2(roomStart.X + x * tileSize, roomStart.Y);
+            Vector2 bottomPos = new Vector2(roomStart.X + x * tileSize, roomStart.Y + (roomHeight - 1) * tileSize);
+
+            //if (topPos != topDoor)  // Don't place brick at the top door
+                //_active.Add(new Block(dungeonTexture, tile, ObjectType.Rock, topPos, 3f));
+
+            //if (bottomPos != bottomDoor) // Don't place brick at the bottom door
+                //_active.Add(new Block(dungeonTexture, tile, ObjectType.Rock, bottomPos, 3f));
+        }
+
+        for (int y = 0; y < roomHeight; y++)
+        {
+            Vector2 leftPos = new Vector2(roomStart.X, roomStart.Y + y * tileSize);
+            Vector2 rightPos = new Vector2(roomStart.X + (roomWidth - 1) * tileSize, roomStart.Y + y * tileSize);
+
+            //if (leftPos != leftDoor) // Don't place brick at the left door
+                //_active.Add(new Block(dungeonTexture, tile, ObjectType.Rock, leftPos, 3f));
+
+            //if (rightPos != rightDoor) // Don't place brick at the right door
+                _active.Add(new Block(dungeonTexture, tile, ObjectType.Rock, rightPos, 3f));
+        }
+    }
+
 
     // Set the active list of blocks
     public void SetActiveList(IBlock newSprite)
