020c02e688ff2ae08e2e7775ddb53b90bfee84e6
DangYuchen <dang.265@osu.edu>
2025-04-21 14:42:05 -0400

    Link.cs And LinkSprite.cs

--- Link1/Link.cs
+++ Link1/Link.cs
@@ -118,7 +118,16 @@ namespace sprint0Test.Link1
         {
             if (isAttacking || isUsingItem) return;
 
-            sprite.SetState(LinkAction.Walking, direction);
+            isMovingThisFrame = true;               
+
+            // 只有在“动作或朝向变化”时才重设动画
+            if (sprite.CurrentAction != LinkAction.Walking ||
+                sprite.CurrentDirection != direction)
+            {
+                sprite.SetState(LinkAction.Walking, direction);
+            }
+
+            // 真正移动
             switch (direction)
             {
                 case LinkDirection.Up: position.Y -= speed; break;
@@ -127,6 +136,8 @@ namespace sprint0Test.Link1
                 case LinkDirection.Right: position.X += speed; break;
             }
         }
+
+
         public void Dash()
         {
             if (!isDashing && dashCooldownCounter <= 0)
@@ -323,6 +334,7 @@ namespace sprint0Test.Link1
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            if (!isVisible) return;
             sprite.Draw(spriteBatch, position);
         }
 
